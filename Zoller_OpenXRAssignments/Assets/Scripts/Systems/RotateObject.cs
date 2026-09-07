b6c17bb8f16f20a95e9b8f83a84425050fbabdbb
cieleux <44616917+cieleux@users.noreply.github.com>
2026-09-07 05:25:53 -0400

    Finished up to the beginning of Part 8.
    Need to fix lighting on candles.

--- Zoller_OpenXRAssignments/Assets/Scripts/Systems/RotateObject.cs
+++ Zoller_OpenXRAssignments/Assets/Scripts/Systems/RotateObject.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class RotateObject : MonoBehaviour
+public sealed class RotateObject : MonoBehaviour
 {
+    /// </summary>
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] 
+    private float rotationSpeed = 250.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
     }
 }
